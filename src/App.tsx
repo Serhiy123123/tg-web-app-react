3165d2bee4a919f44bd2b07c8c0853587333e4ea
Serhiy <sergo0versh@gmail.com>
2025-10-07 15:30:01 +0300

    fixed tg state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useEffect } from "react";
 import "./App.css";
 
+declare global {
+  interface Window {
+    Telegram: any; // или более точный тип, если он у вас есть
+  }
+}
 const tg = window.Telegram.WebApp;
 
 function App() {
   useEffect(() => {
     tg.ready();
-  });
+  }, []);
   const onClose = () => {
     tg.close;
   };
