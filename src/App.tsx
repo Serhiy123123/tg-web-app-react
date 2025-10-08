5c3bee85ebb255ee4b879c60e35d0137a509bc74
Serhiy <sergo0versh@gmail.com>
2025-10-08 07:05:53 +0300

    add form and r-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-// import { useEffect } from "react";
-import { useEffect } from "react";
-import { useTelegram } from "./components/hooks/useTelegram";
 import "./App.css";
-import Header from "./components/Header/Header";
 
-const { tg, onToggleButton } = useTelegram();
+import { useEffect } from "react";
+import { useTelegram } from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import { Route, Routes } from "react-router-dom";
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
+
+const { tg } = useTelegram();
 
 function App() {
   useEffect(() => {
@@ -14,7 +17,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>toggle</button>
+      <Routes>
+        <Route index element={<ProductList />} />
+        <Route path={"form"} element={<Form />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import "./Form.css";
+import { useTelegram } from "../../hooks/useTelegram";
+const Form = () => {
+  const [country, setCountry] = useState("");
+  const [street, setStreet] = useState("");
+  const [subject, setSubject] = useState("physical");
+
+  const { tg } = useTelegram();
+
+  useEffect(() => {
+    tg.MainButton.setParams({
+      text: "Send data",
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!country || !street) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+    }
+  }, [country, street]);
+
+  const onChangeCountry = (e: any) => {
+    setCountry(e.target.value);
+  };
+  const onChangeStreet = (e: any) => {
+    setStreet(e.target.value);
+  };
+  const onChangeSubject = (e: any) => {
+    setSubject(e.target.value);
+  };
+
+  return (
+    <div className={"form"}>
+      <h3>Type your personality</h3>
+      <input
+        className={"input"}
+        type="text"
+        placeholder={"Country"}
+        value={country}
+        onChange={onChangeCountry}
+      />
+      <input
+        className={"input"}
+        type="text"
+        placeholder={"Street"}
+        value={street}
+        onChange={onChangeStreet}
+      />
+      <select className={"select"} value={subject} onChange={onChangeSubject}>
+        <option value={"physical"}>Физ.лицо</option>
+        <option value={"legal"}>Юр.лицо</option>
+      </select>
+    </div>
+  );
+};
+
+export default Form;
